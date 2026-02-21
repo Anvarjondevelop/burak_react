43e4f873d92c9a4528248dfe3d135db60c38c32b
Anvarjon <anvarjondevelop@gmail.com>
2026-02-22 08:43:39 +0900

    feat:  develop productPage nested routing system

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Route, Switch, useRouteMatch } from "react-router-dom";
+
+import Products from "./Products";
+import ChosenProduct from "./ChosenProduct";
 
 function ProductsPage() {
-  return <div>Welcome to ProductsPage</div>;
+  const products = useRouteMatch(); //bu hook filega yo'naltirilgan pathni bilish uchun kerak
+  // console.log("products", products);
+  return (
+    <div className={"products-page"}>
+      <Switch>
+        <Route path={`${products.path}/:productId`}>
+          {/* Nested Routing tizimi  */}
+          <ChosenProduct />
+        </Route>
+        <Route path={`${products.path}`}>
+          <Products />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default ProductsPage;
