6a92a593760026364641e86e4d3c946987abb7bf
Anvarjon <anvarjondevelop@gmail.com>
2026-03-03 22:13:09 +0900

    feat: debug ChosenProduct

--- src/app/screens/productsPage/index.tsx
+++ src/app/screens/productsPage/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Switch, useRouteMatch } from "react-router-dom";
-import { Container } from "@mui/material";
-import ChosenProduct from "./ChosenProduct";
+
 import Products from "./Products";
 import "../../../css/products.css";
+import ChosenProduct from "./ChosenProduct";
 
 function ProductsPage() {
   const products = useRouteMatch();
