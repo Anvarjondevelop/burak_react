da8ceee733432e3cc7495c86ace315d2d1662eae
Anvarjon <anvarjondevelop@gmail.com>
2026-03-12 20:36:14 +0900

    feat: middleware integration redux logger

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,17 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import HomePageReducer from "./screens/homePage/slice";
+import reduxLogger from "redux-logger";
 
 export const store = configureStore({
+  //@ts-ignore
+  middleware: (getDefaultMiddleware) =>
+    //@ts-ignore
+    getDefaultMiddleware().concat(reduxLogger),
   reducer: {
     homePage: HomePageReducer,
   },
 });
+//redux logger store ni ichida qanday ma'lumot bor va  qanday o'zgaryapdi shuni console.log da aniq ko'rsatib beradi
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
